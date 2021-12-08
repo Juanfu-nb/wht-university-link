6cd269e070cc5e30a7af49bd473684d7b069115f
Juanfu-nb <95739626+Juanfu-nb@users.noreply.github.com>
2021-12-08 15:25:59 +0800

    Create 18005.js

---
+++ univ/18005.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "郑州轻工",
+    id: "18005",
+    link: [ {
+        name: "郑轻官网",
+        url: "http://www.zzuli.edu.cn/"
+    }, {
+        name: "快来刷OJ",
+        url: "http://acm.zzuli.edu.cn/"
+    }, {
+        name: "教务系统",
+        url: "http://jiaowu.zzuli.edu.cn/jwweb/home.aspx"
+    }, {
+        name: "数学题库",
+        url: "http://202.196.1.67/jpkc/"
+    }, {
+        name: "图书馆",
+        url: "http://lib.zzuli.edu.cn/"
+    }, {
+        name: "VPN",
+        url: "https://webvpn.zzuli.edu.cn/login"
+    }, {
+        name: "蕉个朋友",
+        url: "http://wpa.qq.com/msgrd?v=3&uin=1535500805&site=qq&menu=yes"
+    } ]
+};
